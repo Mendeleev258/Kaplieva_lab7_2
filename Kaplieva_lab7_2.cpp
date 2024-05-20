24d8472d7f355b11864969c9e7b0b64bce5afed8
junker <jroslavl.2005@gmail.com>
2024-05-21 00:30:35 +0300

    Done

--- Kaplieva_lab7_2.cpp
+++ Kaplieva_lab7_2.cpp
@@ -2,31 +2,59 @@
 #include <fstream>
 #include "Flist.h"
 
-bool task(FLIST& list);
+bool add_sizes(FLIST& list);
+void task(std::ifstream& input, std::ofstream& output);
 
 int main()
 {
 	std::ifstream file("file.txt");
-	FLIST list;
-	list.create_by_queue(file);
-	list.print("");
+	std::ofstream output_file("output.txt");
+	task(file, output_file);
 }
 
-bool task(FLIST& list)
+bool add_sizes(FLIST& list)
 {
+	bool result{ false };
 	TInfo cnt{ 1 };
-	ptrNODE ptr = list.get_head()->next;
-	while (ptr)
+	ptrNODE ptr = list.get_head();
+	ptrNODE tmp_ptr = nullptr;
+	while (ptr->next->next)
 	{
-		if (ptr->info == ptr->next->info)
+		if (ptr->next->info == ptr->next->next->info)
 		{
-			ptrNODE tmp_ptr = ptr;
+			if (!tmp_ptr)
+				tmp_ptr = ptr;
 			cnt++;
+			result = true;
 		}
 		else
 		{
-			cnt = 1;
-
+			if (cnt > 1)
+			{
+				list.add_after(tmp_ptr, cnt);
+				cnt = 1;
+				tmp_ptr = nullptr;
+			}
 		}
+		ptr = ptr->next;
 	}
+	if (cnt > 0)
+		list.add_after(tmp_ptr, cnt);
+	return result;
+}
+
+void task(std::ifstream& input, std::ofstream& output)
+{
+	FLIST list;
+	list.create_by_queue(input);
+	bool no_flag = !add_sizes(list);
+	ptrNODE ptr = list.get_head()->next;
+	while (ptr)
+	{
+		output << ptr->info << ' ';
+		ptr = ptr->next;
+	}
+	if (no_flag)
+		output << "\nNO!";
+
 }
