f34283947db6e56f3c66d8d9afb36b7b77f17808
Yaroslav <121808013+Mendeleev258@users.noreply.github.com>
2024-05-20 19:13:57 +0300

    Добавьте файлы проекта.

---
+++ Kaplieva_lab7_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include "Flist.h"
+
+bool task(FLIST& list);
+
+int main()
+{
+	std::ifstream file("file.txt");
+	FLIST list;
+	list.create_by_queue(file);
+	list.print("");
+}
+
+bool task(FLIST& list)
+{
+	TInfo cnt{ 1 };
+	ptrNODE ptr = list.get_head()->next;
+	while (ptr)
+	{
+		if (ptr->info == ptr->next->info)
+		{
+			ptrNODE tmp_ptr = ptr;
+			cnt++;
+		}
+		else
+		{
+			cnt = 1;
+
+		}
+	}
+}
